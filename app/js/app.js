6597765e87626c34b66feab53c25835ea3eb82ba
TrywaR <TrywaR@yandex.ru>
2021-11-17 16:46:00 +0300

    webpack conf

---
+++ app/js/app.js
@@ -0,0 +1,3 @@
+// import fontawesome from '@fortawesome/fontawesome-free'
+import '@fortawesome/fontawesome-free/js/all.min.js';
+import "../sass/app.sass";
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './app/js/app.js',
+  module: {
+    rules: [
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      { test: /\.svg$/, use: 'svg-inline-loader' },
+      { test: /\.(jpg|jpeg|png)$/, use: 'file-loader' },
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({'template': './app/app.html'}),
+    new CleanWebpackPlugin()
+  ],
+  devServer: {
+    port: 666
+  },
+  performance: {
+   hints: false,
+   maxEntrypointSize: 5120000,
+   maxAssetSize: 5120000
+  },
+  devtool: false,
+  mode: 'production'
+}
